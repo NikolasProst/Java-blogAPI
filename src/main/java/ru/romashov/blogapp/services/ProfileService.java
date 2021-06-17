f8c5ee607ec45342991a264c7106a228952f8c7e
Ромашов Николай Евгеньевич <NERomashov@sberbank.ru>
2021-06-17 16:46:56 +0300

    update database for deploy

--- src/main/java/ru/romashov/blogapp/services/ProfileService.java
+++ src/main/java/ru/romashov/blogapp/services/ProfileService.java
@@ -1,6 +1,7 @@
 package ru.romashov.blogapp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ public class ProfileService {
     @Autowired
     private FileSystemStorageService storageService;
 
+    @Lazy
     @Autowired
     private PasswordEncoder passwordEncoder;
 
